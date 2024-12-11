ebab8dc46f4ad3241b3d7666f43dfd78b45b2ec9
Wagner <wagnersanches5@hotmail.com>
2024-12-11 14:04:56 -0300

    Initializr init

---
+++ src/test/java/com/spring/oath2/Example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.oath2.Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
